54d96f57a49debb3281e67f124dc7ef0186c886d
scippio <scippio@2ants.eu>
2017-01-15 23:01:40 +0100

    own debug config

--- src/CellpackI18n.ts
+++ src/CellpackI18n.ts
@@ -7,9 +7,16 @@ import { Cellpack, Connection, Transmitter } from "microb"
 export default class CellpackI18n extends Cellpack {
 
     private i18n: any
+    // own config - TODO: own structure?
+    private debug: boolean
+    // defaultLocale: string
 
     init(){
+        // own config
         this.config = this.environment.get("cellpacks")["cellpack-i18n"]
+        // debug
+        if(this.config.debug !== undefined) this.debug = this.config.debug
+        else this.config = this.environment.get('debug',false)
 
         this.i18n = {}
         I18n.configure({
@@ -28,12 +35,12 @@ export default class CellpackI18n extends Cellpack {
     }
 
     translations(text: string, subs?: any){
-        if(this.environment.get('debug')) this.transmitter.emit("log.cellpack.i18n",`Translation of: "${text}" with: ${subs}`)
+        if(this.config) this.transmitter.emit("log.cellpack.i18n",`Translation of: "${text}" with: ${subs}`)
         try {
             return this.i18n.__(text,subs)
         } catch(err){
-            if(this.environment.get('debug')){
-                this.transmitter.emit("log.cellpack.i18n",`Translation Error: "${err}" in: ${text} with: ${subs}`)
+            this.transmitter.emit("log.cellpack.i18n",`Translation Error: "${err}" in: ${text} with: ${subs}`)
+            if(this.config){
                 return `<div class="microb-error" style="color:red; background-color:#fee; border:solid 1px #f00; border-radius:3px; padding:10px; font-size:1em; font-family:Monospace"><strong>Translation error: </strong>${err}<br><strong>In: </strong>${text}<br><strong>With: </strong>${subs}</div>`
             }
         }
@@ -42,7 +49,7 @@ export default class CellpackI18n extends Cellpack {
     request(connection: Connection){
         if(connection.request.attributes.has("locale")) this.i18n.setLocale(connection.request.attributes.get("locale"))
         if(connection.environment.has("cellpack.i18n.locale")) this.i18n.setLocale(connection.environment.get("cellpack.i18n.locale"))
-        if(this.environment.get('debug')) this.transmitter.emit("log.cellpack.i18n",`Set Locale: ${this.i18n.getLocale()}`)
+        if(this.debug) this.transmitter.emit("log.cellpack.i18n",`Set Locale: ${this.i18n.getLocale()}`)
         return Promise.resolve(true)
     }
 }
--- types/CellpackI18n.d.ts
+++ types/CellpackI18n.d.ts
@@ -3,6 +3,7 @@ import * as Promise from "bluebird";
 import { Cellpack, Connection } from "microb";
 export default class CellpackI18n extends Cellpack {
     private i18n;
+    private debug;
     init(): Promise<void>;
     translations(text: string, subs?: any): any;
     request(connection: Connection): Promise<boolean>;
